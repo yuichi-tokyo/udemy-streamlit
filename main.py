6a8e843572af69cf5f0a5cd782d71c2cd1054d4d
yuichi-tokyo <pimp1116tokyoheads@gmail.com>
2021-04-21 18:53:32 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import time
+
+st.title('Steamlit 超入門')
+
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Interation {i + 1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'Done!!!!!'
+
+left_column, right_column = st.beta_columns(2)
+button = left_column.button('右カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander1 = st.beta_expander('問い合わせ1')
+expander1.write('問い合わせ1の回答')
+expander2 = st.beta_expander('問い合わせ1')
+expander2.write('問い合わせ2の回答')
+expander3 = st.beta_expander('問い合わせ3')
+expander3.write('問い合わせ3の回答')
